94764f040cd6db70c8b72eae9a055ecfb3df09f4
mel-miguel <128431446+mel-miguel@users.noreply.github.com>
2023-04-17 20:54:11 -0300

    aula2
    aula 2

---
+++ aula2.js
@@ -0,0 +1,6 @@
+var nome = prompt("Informe seu nome");
+var idade = prompt("Informe sua idade");
+var local = prompt("Informe seu Local");
+alert("Seu nome é " + nome + " e voce tem " + idade + " anos " + "e voce mora em " + local);
+document.write("Seu nome é " + nome + " e voce tem " + idade + " anos " + " e voce mora em " + local)
+document.write(" Esse é o seu primeiro site " + nome);
