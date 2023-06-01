437c1068df9291f7d06be31f7e0a9a886e0ccac4
mel-miguel <128431446+mel-miguel@users.noreply.github.com>
2023-05-31 21:01:57 -0300

    a
    b

---
+++ aula8.js
@@ -0,0 +1,15 @@
+"Exercicio 1"
+
+var Herois = [];
+
+for( var c = 0; c <= 5; c = c +1) {
+    
+    Herois[c] = prompt ("Nome do Heroi da Marvel " + c + "?");
+    alert(Herois[c]);
+}
+
+for( var c = 0; c <= 5; c = c +1){
+
+    alert("O nome do Heroi " + c + " e " + Herois[c] )
+}
+
